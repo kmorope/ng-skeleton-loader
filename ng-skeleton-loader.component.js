c78541ba0b187bff5cc59d86da6c3f205804f8b8
Camilo Romero <kmorope@outlook.com>
2020-01-28 22:02:41 -0500

    Removed template, generate loader on code

--- ng-skeleton-loader.component.js
+++ ng-skeleton-loader.component.js
@@ -1,36 +1,73 @@
 'use strict';
 
-angular.module('skeletonLoaderModule').
+angular.module('skeletonLoaderModule', []).
     component('skeletonLoader', {
-        templateUrl:'/node_modules/ng-skeleton-loader/ng-skeleton-loader.template.html',
         bindings: {
             type: '=',
             size: '=?',
             columns: '=?',
             rows: '=?',
-            fields: '=?'
+            fields: '=?',
+            largeText:'=?'
         },
         controllerAs: 'skCtrl',
-        controller: function () {
+        controller: function ($scope, $element) {
             let self = this;
             self.$onInit = function () {
                 // Load default values             
-                self.size = self.size || 's';
-                self.columns = self.columns || 6;
-                self.fields = self.fields || 2;
+                // self.size = self.size || 's';
+                self.type = self.type || 'form';
+                self.columns = self.columns || 1;
+                self.fields = self.fields || 1;
                 self.rows = (self.columns * self.rows) || (self.columns * 2);
+                self.largeText = (self.largeText === 'true') || false;
+                self.createElement();
             }
-            self.colClass = function () {
-                switch (self.columns) {
-                    case 2:
-                        return 'two-col';
-                    case 4:
-                        return 'four-col';
-                    case 6:
-                        return 'six-col';
+
+            self.createElement = function(){
+                let main = document.createDocumentFragment();
+                let container = document.createElement('div');
+                container.classList.add('skeleton');                
+                container.classList.add(self.type);
+                self.evalType(container);           
+                self.evalColums(container);    
+                main.appendChild(container);
+                $element[0].appendChild(main);
+            }
+
+            self.evalType = (container) => {
+                switch (self.type) {
+                    case 'form':                        
+                        createFormItems(container);
+                        break;                
                     default:
-                        return 'two-col';
+                        createGridItems(container);
+                        break;
                 }
+            }
+            
+            self.evalColums = (container) => {
+                container.setAttribute('style','grid-template-columns: repeat(' + self.columns + ',1fr) !important;');
+            }
+
+            function createFormItems(container){
+                for (let index = 0; index < self.fields; index++) {
+                    let elementContainer = document.createElement('div');
+                    elementContainer.classList.add('sk-group');
+                    let label = document.createElement('div');
+                    label.classList.add('sk-base');
+                    label.classList.add('sk-label');
+                    let input = document.createElement('div');
+                    input.classList.add('sk-base');
+                    input.classList.add(self.largeText === true && index === 0 ? 'sk-big-input' : 'sk-input');
+                    elementContainer.appendChild(label);
+                    elementContainer.appendChild(input);
+                    container.appendChild(elementContainer);
+                }
+            }
+
+            function createGridItems(container) {
+                return true;
             }
         }
     });
